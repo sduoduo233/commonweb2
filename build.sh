26153d1f6d7e81a5d71347ad791a134c2a421ae9
duoduo song <sduoduo233@gmail.com>
2024-01-13 22:17:58 +0800

    add file extension for windows builds

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -trimpath -ldflags "-s -w" -o bui
 GOOS=linux GOARCH=386 CGO_ENABLED=0 go build -trimpath -ldflags "-s -w" -o build/commonweb2-linux-386
 GOOS=linux GOARCH=arm64 CGO_ENABLED=0 go build -trimpath -ldflags "-s -w" -o build/commonweb2-linux-arm64
 
-GOOS=windows GOARCH=amd64 CGO_ENABLED=0 go build -trimpath -ldflags "-s -w" -o build/commonweb2-windows-amd64
-GOOS=windows GOARCH=386 CGO_ENABLED=0 go build -trimpath -ldflags "-s -w" -o build/commonweb2-windows-386
-GOOS=windows GOARCH=arm64 CGO_ENABLED=0 go build -trimpath -ldflags "-s -w" -o build/commonweb2-windows-arm64
+GOOS=windows GOARCH=amd64 CGO_ENABLED=0 go build -trimpath -ldflags "-s -w" -o build/commonweb2-windows-amd64.exe
+GOOS=windows GOARCH=386 CGO_ENABLED=0 go build -trimpath -ldflags "-s -w" -o build/commonweb2-windows-386.exe
+GOOS=windows GOARCH=arm64 CGO_ENABLED=0 go build -trimpath -ldflags "-s -w" -o build/commonweb2-windows-arm64.exe
