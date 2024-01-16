57099544798dc53f7cb25f5219212af5ba9616f8
duoduo song <sduoduo233@gmail.com>
2024-01-16 16:53:22 +0800

    use default values from http.DefaultTransport

--- client/client.go
+++ client/client.go
@@ -10,6 +10,7 @@ import (
 	"log/slog"
 	"net"
 	"net/http"
+	"time"
 
 	utls "github.com/refraction-networking/utls"
 )
@@ -47,6 +48,18 @@ func NewClient(up, down, listen string, useUTLS bool) *client {
 
 				return uConn, nil
 			},
+
+			// default values from http.DefaultTransport
+			Proxy: http.ProxyFromEnvironment,
+			DialContext: (&net.Dialer{
+				Timeout:   30 * time.Second,
+				KeepAlive: 30 * time.Second,
+			}).DialContext,
+			ForceAttemptHTTP2:     true,
+			MaxIdleConns:          100,
+			IdleConnTimeout:       90 * time.Second,
+			TLSHandshakeTimeout:   10 * time.Second,
+			ExpectContinueTimeout: 1 * time.Second,
 		}
 	}
 	return &client{
