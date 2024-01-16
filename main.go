80eaade41aa49a89885992db70d7cabf03c38c59
duoduo song <sduoduo233@gmail.com>
2024-01-16 19:46:07 +0800

    include line number in logs

--- main.go
+++ main.go
@@ -29,7 +29,8 @@ func main() {
 	}
 
 	slog.SetDefault(slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
-		Level: logLevel,
+		Level:     logLevel,
+		AddSource: true,
 	})))
 
 	slog.Info("commonweb2", "mode", *mode)
